74d24b8b1d82579f34a4df68f5d3ef76645894d8
Naciongsc <60244165+Naciongsc@users.noreply.github.com>
2020-10-01 11:23:59 +0800

    Update simpleserver.js

--- simpleserver.js
+++ simpleserver.js
@@ -9,27 +9,3 @@ const server = http.createServer((req, res) => {
 	res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-	console.log('Server running at http://${hostname}:${port}/');
-});
-
-const http = require('http');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
-
-const mimeTypes = {
-"html": "text/html",
-"jpeg": "image/jpeg",
-"jpg": "image/jpg",
-"png": "image/png"
-"js": "text/javascript",
-"css": "text/css"
-};
-
-http.createServer(function(req, res) {
-var uri = url.parse(req.url).pathname;
-var fileName = path.join(process.cwd(), unescape(uri));
-console.log('Loading'+uri);
-var stats;
-});
